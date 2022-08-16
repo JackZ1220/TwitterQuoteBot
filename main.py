9a1d791ce0824ea0664e3494f12a7f2db360e19b
Jack Zhong <92420123+JackZ1220@users.noreply.github.com>
2022-08-15 23:00:59 -0400

    Update main.py
    Added tweet pulling function. Pulls latest 100 tweets relating to 'keyword' which is set to 'stock market'. Runs all tweets through sentiment analysis and averages all the values, then prints it out.

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
 from urllib import response
 import config
+import pandas as pd
 import tweepy
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 
 # Authenticate with Twitter
-client  = tweepy.Client(consumer_key=config.api_key,
-                        consumer_secret=config.api_secret,
-                        access_token=config.access_token,
-                        access_token_secret=config.access_secret)
+auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
+auth.set_access_token(config.access_token, config.access_secret)
 
-# Get the user's most recent tweet
-tweet = 'Hello World! @realDonaldTrump'
+# Create API object
+api = tweepy.API(auth)
 
-#preprocess the tweet
-tweet_words = []
+# Search for Tweets
 
-for word in tweet.split(' '):
-    if word.startswith('@') and len(word) > 1:
-        word = '@user'
-    elif word.startswith('http'):
-        word = 'http'
-    tweet_words.append(word)
+keywords = 'stock market'
+limit = 100
 
-tweet_processed = " ".join(tweet_words)
+tweets = tweepy.Cursor(api.search_tweets, q=keywords, count=100, tweet_mode='extended').items(limit)
+
+
+# Create a dataframe with the Tweets
+columns = ['User', 'Tweet']
+data = []
+
+for tweet in tweets:
+    data.append([tweet.user.screen_name, tweet.full_text])
+
+df = pd.DataFrame(data, columns=columns)
+
 
 #load the NLP model and tokenizer (roberta)
 roberta = "cardiffnlp/twitter-roberta-base-sentiment"
 model = AutoModelForSequenceClassification.from_pretrained(roberta)
 tokenizer = AutoTokenizer.from_pretrained(roberta)
 lables = ['Negative', 'Neutral', 'Positive']
+scores_totals = [0, 0, 0]
 
-# Analyze the tweet
-encoded_tweet = tokenizer(tweet_processed, return_tensors='pt')
-output = model(**encoded_tweet)
 
-scores = output[0][0].detach().numpy()
-scores = softmax(scores)
-for i in range(len(scores)):
+# Running through dataframe and predicting sentiment
+for index, row in df.iterrows():
 
-    l = lables[i]
-    s = scores[i]
-    print(l, s)
+    #preprocess the tweet
+    tweet_words = []
+    tweet = row['Tweet']
+    for word in tweet.split(' '):
+        if word.startswith('@') and len(word) > 1:
+            word = '@user'
+        elif word.startswith('http'):
+            word = 'http'
+        tweet_words.append(word)
+
+    tweet_processed = " ".join(tweet_words)
+
+    # Analyze the tweet
+    encoded_tweet = tokenizer(tweet_processed, return_tensors='pt')
+    output = model(**encoded_tweet)
+
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    for i in range(len(scores)):
+
+        scores_totals[i] += scores[i]
+
+for i in range(len(scores_totals)):
+    scores_totals[i] = scores_totals[i] / limit
+    print(lables[i] + ": " + str(scores_totals[i]))
+
+for i in range(len(scores_totals)):
+    if scores_totals[i] > 0.5:
+        print("The past 100 tweets relating to the Stock Market are mostly " + lables[i])
+        
